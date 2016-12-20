ca9ee0e362266c65b762cc6d75afb4aabc6a8a08
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2016-12-20 13:53:04 +0800

    fix redefine the primary key column 'id'

--- test/seeds.rb
+++ test/seeds.rb
@@ -2,13 +2,11 @@ ActiveRecord::Schema.define do
   self.verbose = false
 
   create_table :users, :force => true do |t|
-    t.integer :id
     t.string :name
     t.string :email
     t.text :serialized_attribute
   end
   create_table :posts, :force => true do |t|
-    t.integer :id
     t.integer :user_id
     t.string :title
   end
