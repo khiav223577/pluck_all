8cd0bccd46d8b95eb563ef9fe9018db77170f134
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2016-12-20 13:13:46 +0800

    add test model

--- test/pluck_all_test.rb
+++ test/pluck_all_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class PluckAllTest < Minitest::Test
+	def setup
+    @user = User.create(:name => 'John', :serialized_attribute => {:fb_id => 141242512})
+  end
   def test_that_it_has_a_version_number
     refute_nil ::PluckAll::VERSION
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,29 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pluck_all'
 
 require 'minitest/autorun'
+
+
+ActiveRecord::Base.establish_connection(
+  "adapter"  => "sqlite3",
+  "database" => ":memory:"
+)
+ActiveRecord::Schema.define do
+  self.verbose = false
+
+  create_table :users, :force => true do |t|
+    t.integer :id
+    t.string :name
+    t.text :serialized_attribute
+  end
+  create_table :posts, :force => true do |t|
+    t.integer :id
+    t.string :title
+  end
+end
+class User < ActiveRecord::Base
+  serialize :serialized_attribute, Hash
+  has_many :posts
+end
+class Post < ActiveRecord::Base
+  belongs_to :user
+end
