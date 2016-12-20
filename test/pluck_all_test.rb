cbe3ac7c03fba3fda1596047cd19050300e71dae
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2016-12-20 13:44:53 +0800

    fix indent

--- test/pluck_all_test.rb
+++ test/pluck_all_test.rb
@@ -1,34 +1,34 @@
 require 'test_helper'
 
 class PluckAllTest < Minitest::Test
-	def setup
+  def setup
     
   end
   def test_that_it_has_a_version_number
     refute_nil ::PluckAll::VERSION
   end
   def test_pluck_one_column
-  	assert_equal User.pluck_all(:name), [{'name' => 'John'}, {'name' => 'Pearl'}, {'name' => 'Kathenrie'}]
+    assert_equal User.pluck_all(:name), [{'name' => 'John'}, {'name' => 'Pearl'}, {'name' => 'Kathenrie'}]
   end
   def test_pluck_multiple_column
-  	assert_equal User.pluck_all(:name, :email), [
-  		{'name' => 'John', 'email' => 'john@example.com'}, 
-  		{'name' => 'Pearl', 'email' => 'pearl@example.com'}, 
-  		{'name' => 'Kathenrie', 'email' => 'kathenrie@example.com'},
-  	]
+    assert_equal User.pluck_all(:name, :email), [
+      {'name' => 'John', 'email' => 'john@example.com'}, 
+      {'name' => 'Pearl', 'email' => 'pearl@example.com'}, 
+      {'name' => 'Kathenrie', 'email' => 'kathenrie@example.com'},
+    ]
   end
   def test_pluck_serialized_attribute
-  	assert_equal User.where(:name => %w(John Pearl)).pluck_all(:serialized_attribute), [{'serialized_attribute' => {}}, {'serialized_attribute' => {:testing => true, :deep => {:deep => :deep}}}]
+    assert_equal User.where(:name => %w(John Pearl)).pluck_all(:serialized_attribute), [{'serialized_attribute' => {}}, {'serialized_attribute' => {:testing => true, :deep => {:deep => :deep}}}]
   end
 
   def test_join_and_alias_column
-  	assert_equal User.joins(:posts).where(:name => 'John').pluck_all(:name, :title), [
-  		{'name' => 'John', 'title' => 'John\'s post1'},
-  		{'name' => 'John', 'title' => 'John\'s post2'},
-  		{'name' => 'John', 'title' => 'John\'s post3'},
-  	]
-  	assert_equal User.joins(:posts).where(:name => 'Pearl').pluck_all(:'name AS user_name', :'title AS post_title'), [
-  		{'user_name' => 'Pearl', 'post_title' => 'Pearl\'s post1'},
-  	]
+    assert_equal User.joins(:posts).where(:name => 'John').pluck_all(:name, :title), [
+      {'name' => 'John', 'title' => 'John\'s post1'},
+      {'name' => 'John', 'title' => 'John\'s post2'},
+      {'name' => 'John', 'title' => 'John\'s post3'},
+    ]
+    assert_equal User.joins(:posts).where(:name => 'Pearl').pluck_all(:'name AS user_name', :'title AS post_title'), [
+      {'user_name' => 'Pearl', 'post_title' => 'Pearl\'s post1'},
+    ]
   end
 end
--- test/seeds.rb
+++ test/seeds.rb
@@ -1,11 +1,11 @@
 users = User.create([
-	{:name => 'John', :email => 'john@example.com'},
-	{:name => 'Pearl', :email => 'pearl@example.com', :serialized_attribute => {:testing => true, :deep => {:deep => :deep}}},
-	{:name => 'Kathenrie', :email => 'kathenrie@example.com'},
+  {:name => 'John', :email => 'john@example.com'},
+  {:name => 'Pearl', :email => 'pearl@example.com', :serialized_attribute => {:testing => true, :deep => {:deep => :deep}}},
+  {:name => 'Kathenrie', :email => 'kathenrie@example.com'},
 ])
 Post.create([
-	{:title => 'John\'s post1', :user_id => users[0].id},
-	{:title => 'John\'s post2', :user_id => users[0].id},
-	{:title => 'John\'s post3', :user_id => users[0].id},
-	{:title => 'Pearl\'s post1', :user_id => users[1].id},
+  {:title => 'John\'s post1', :user_id => users[0].id},
+  {:title => 'John\'s post2', :user_id => users[0].id},
+  {:title => 'John\'s post3', :user_id => users[0].id},
+  {:title => 'Pearl\'s post1', :user_id => users[1].id},
 ])
