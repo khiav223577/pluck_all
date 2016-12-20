c9dee6bde10f38f26e533af0178f20662bba9102
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2016-12-20 13:38:10 +0800

    test serialize attribute

--- test/pluck_all_test.rb
+++ test/pluck_all_test.rb
@@ -7,7 +7,10 @@ class PluckAllTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::PluckAll::VERSION
   end
-  def test_pluck_names
+  def test_pluck_one_column
   	assert_equal User.pluck_all(:name), [{'name' => 'John'}, {'name' => 'pearl'}, {'name' => 'kathenrie'}]
+  end
+  def test_pluck_serialized_attribute
+  	assert_equal User.where(:name => %w(John pearl)).pluck_all(:serialized_attribute), [{'serialized_attribute' => {}}, {'serialized_attribute' => {:testing => true, :deep => {:deep => :deep}}}]
   end
 end
--- test/seeds.rb
+++ test/seeds.rb
@@ -1,5 +1,5 @@
 User.create([
 	{:name => 'John', :email => 'john@example.com'},
-	{:name => 'pearl', :email => 'pearl@example.com'},
+	{:name => 'pearl', :email => 'pearl@example.com', :serialized_attribute => {:testing => true, :deep => {:deep => :deep}}},
 	{:name => 'kathenrie', :email => 'kathenrie@example.com'},
 ])
