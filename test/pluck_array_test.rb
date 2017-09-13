4528f8acd94f62cb296bd2fbd1e1057bb1f39ccc
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-09-13 16:09:29 +0800

    test join with table name and test alias

--- test/pluck_array_test.rb
+++ test/pluck_array_test.rb
@@ -4,9 +4,11 @@ class PluckArrayTest < Minitest::Test
   def setup
     
   end
+  
   def test_pluck_one_column
     assert_equal(["John", "Pearl", "Kathenrie"], User.pluck_array(:name))
   end
+
   def test_pluck_multiple_columns
     assert_equal([
       ["John", "john@example.com"],
@@ -14,12 +16,14 @@ class PluckArrayTest < Minitest::Test
       ["Kathenrie", "kathenrie@example.com"],
     ], User.pluck_array(:name, :email))
   end
+
   def test_pluck_serialized_attribute
     assert_equal([
       {}, 
       {:testing => true, :deep => {:deep => :deep}},
     ], User.where(:name => %w(John Pearl)).pluck_array(:serialized_attribute))
   end
+
   def test_join
     assert_equal([
       ['John', "John's post1"],
@@ -27,17 +31,19 @@ class PluckArrayTest < Minitest::Test
       ['John', "John's post3"],
     ], User.joins(:posts).where(:name => 'John').pluck_array(:name, :title))
   end
-  # def test_join_with_table_name #Rails 5 doesn't support this..
-  #   assert_equal([
-  #     ['John', "John's post1"],
-  #     ['John', "John's post2"],
-  #     ['John', "John's post3"],
-  #   ], User.joins(:posts).where(:name => 'John').pluck_array(:'users.name', :'posts.title'))
-  # end
-  # def test_alias #Rails 5 doesn't support this..
-  #   assert_equal([
-  #     ["Pearl", "Pearl's post1"],
-  #     ["Pearl", "Pearl's post2"],
-  #   ], User.joins(:posts).where(:name => 'Pearl').pluck_array(:'name AS user_name', :'title AS post_title'))
-  # end
+
+  def test_join_with_table_name
+    assert_equal([
+      ['John', "John's post1"],
+      ['John', "John's post2"],
+      ['John', "John's post3"],
+    ], User.joins(:posts).where(:name => 'John').pluck_array(:'users.name', :'posts.title'))
+  end
+
+  def test_alias
+    assert_equal([
+      ["Pearl", "Pearl's post1"],
+      ["Pearl", "Pearl's post2"],
+    ], User.joins(:posts).where(:name => 'Pearl').pluck_array('users.name AS user_name', 'title AS post_title')) # Rails 5 cannot use symbol + AS
+  end
 end
