4e0bb5e0c08128f9f41452b31e525af651d76525
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2021-06-11 01:22:09 +0800

    implement auto join

--- test/active_record/globalize_test.rb
+++ test/active_record/globalize_test.rb
@@ -12,4 +12,19 @@ class ActiveRecordGlobalizeTest < Minitest::Test
       { 'title' => '你最愛的食物為何？' },
     ], Questionnaire.with_translations(:'zh-TW').pluck_all(:title))
   end
+
+  def test_pluck_all_with_auto_join
+    I18n.with_locale(:en) do
+      assert_equal([
+        { 'title' => 'What is your favorite food?' },
+        { 'title' => 'Why did you purchase this product?' },
+      ], Questionnaire.pluck_all(:title))
+    end
+
+    I18n.with_locale(:'zh-TW') do
+      assert_equal([
+        { 'title' => '你最愛的食物為何？' },
+      ], Questionnaire.pluck_all(:title))
+    end
+  end
 end
