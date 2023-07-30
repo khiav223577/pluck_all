300047ea29021558c3bfe54ca1492548fdfed762
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2023-07-30 16:59:03 +0800

    Fix: uninitialized constant User
    In the test setting Test (MONGOID, 3.1, mongoid_73.gemfile)
    
    got this error:
    SimpleCov failed with exit 1/home/runner/work/pluck_all/pluck_all/test/mongoid/support/seeds.rb:2:in `<top (required)>': uninitialized constant User (NameError)

--- test/mongoid/support/seeds.rb
+++ test/mongoid/support/seeds.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
+
+require 'rails_compatibility/setup_autoload_paths'
+RailsCompatibility.setup_autoload_paths [File.expand_path('../models/', __FILE__)]
+
 User.delete_all
 User.create(name: 'Pearl Shi', age: 18)
 User.create(name: 'Rumble Huang', age: 20)
