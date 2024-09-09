3ba6c37ca7cf2b7a7c2628feb9212e07bb3d913a
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2024-09-09 12:13:37 +0800

    Fix: `serialize': wrong number of arguments (given 2, expected 1)

--- test/active_record/support/models/user.rb
+++ test/active_record/support/models/user.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 class User < ActiveRecord::Base
-  serialize :serialized_attribute, Hash
+  if Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new('7.1.0')
+    serialize :serialized_attribute, Hash
+  else
+    serialize :serialized_attribute, type: Hash
+  end
+
   mount_uploader :profile_pic, ProfilePictureUploader
   mount_uploader :pet_pic, PetPictureUploader
   has_many :posts
